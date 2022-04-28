e01c6e2df6cbdd3adc1247c7a40401e905409999
togami2864 <tuabobo123@gmail.com>
2022-04-28 21:37:04 +0900

    add transform middleware

--- micro-vite/src/dev.ts
+++ micro-vite/src/dev.ts
@@ -1,14 +1,21 @@
 import connect from "connect";
 import historyApiFallback from "connect-history-api-fallback";
 import sirv from "sirv";
+import { transformMiddleware } from "./transformMiddleware";
 
 export const startDev = () => {
   const server = connect();
   server.listen(3000, "localhost");
+  server.use(transformMiddleware());
   server.use(
     sirv(undefined, {
       dev: true,
       etag: true,
+      setHeaders(res, pathname) {
+        if (/\.[tj]s$/.test(pathname)) {
+          res.setHeader("Content-Type", "application/javascript");
+        }
+      },
     })
   );
   server.use(historyApiFallback() as any);
