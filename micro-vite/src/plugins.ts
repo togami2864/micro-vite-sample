7019cc37dbdcba176b23250d6c7ce66e13045a35
togami2864 <tuabobo123@gmail.com>
2022-04-28 23:19:03 +0900

    add reload sctipt

--- micro-vite/src/plugins.ts
+++ micro-vite/src/plugins.ts
@@ -1,8 +1,10 @@
 import { resolve } from "./resolvePlugin";
 import esbuild from "rollup-plugin-esbuild";
+import { reload } from "./reloadPlugin";
 
 export const getPlugins = () => [
   resolve(),
+  reload(),
   esbuild({
     target: "esnext",
     minify: false,
